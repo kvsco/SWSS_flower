6e6b5aae22825063b2b4e24268548aeae8b73aea
kvsco <nngjk@naver.com>
2021-10-03 20:30:12 +0900

    connect urls

--- flower/urls.py
+++ flower/urls.py
@@ -1,7 +1,6 @@
 """config URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,15 +14,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import handler403, url, include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #     앱명          urls 경로 명시
-    path('', include('flower.urls')),
-    # 400번대 url오류 명시할 사이트
-    # handler400 = ''
-    # handler403 = ''
-    # handler404 = ''
-    # handler500 = ''
+    path('', views.index, name='index'),
+
+    #path('<str:num>/', views.num, name='num'),
 ]
--- flower/views.py
+++ flower/views.py
@@ -1,3 +1,8 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    print("----"*20)
+
+    return HttpResponse("<h1>index</h1>")
